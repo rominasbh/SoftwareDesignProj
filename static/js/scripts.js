92b277ac85e1cdba9694fdc1c8388351a0b3cc2c
Bolu <oo222515@gmail.com>
2024-03-30 11:49:22 -0500

    fixed date, gallon validation

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -22,32 +22,49 @@ function validateForm() {
 }
 
 function updateFuelQuote() {
-  const gallons = parseFloat(fuelGallons.value);
-  const delivery_date = delivery_dateInput.value;
+  const gallon_value = fuelGallons.value.trim();
+  const deliveryDateValue = delivery_dateInput.value.trim();
 
-  //delivery fee
-  const deliveryFee = 1.75;
+  if (
+    gallon_value != "" &&
+    parseFloat(gallon_value) > 0 &&
+    deliveryDateValue != ""
+  ) {
+    const gallons = parseFloat(gallon_value);
+    const delivery_date = deliveryDateValue;
 
-  //base price calulation
-  const pricePerGallon = 3.05;
-  const basePrice = pricePerGallon * gallons;
-  const estimatedCost = basePrice;
-  console.log(basePrice);
-  //tax fee
-  const taxRate = 0.0775;
-  const taxFee = basePrice * taxRate;
+    //delivery fee
+    const deliveryFee = 1.75;
 
-  //total price
-  const totalPrice = deliveryFee + taxFee + basePrice;
+    //base price calulation
+    const pricePerGallon = 3.05;
+    const basePrice = pricePerGallon * gallons;
+    const estimatedCost = basePrice;
+    console.log(basePrice);
+    //tax fee
+    const taxRate = 0.0775;
+    const taxFee = basePrice * taxRate;
 
-  //update display
-  costDisplay.innerHTML = `${estimatedCost.toFixed(2)}`;
-  price.innerHTML = `${pricePerGallon.toFixed(2)}`;
-  delivery.innerHTML = `${deliveryFee.toFixed(2)}`;
-  total.innerHTML = `${totalPrice.toFixed(2)}`;
-  tax.innerHTML = `${taxFee.toFixed(2)}`;
-  shownGallons.innerHTML = `${gallons.toFixed(2)}`;
+    //total price
+    const totalPrice = deliveryFee + taxFee + basePrice;
 
+    //update display
+    costDisplay.innerHTML = `${estimatedCost.toFixed(2)}`;
+    price.innerHTML = `${pricePerGallon.toFixed(2)}`;
+    delivery.innerHTML = `${deliveryFee.toFixed(2)}`;
+    total.innerHTML = `${totalPrice.toFixed(2)}`;
+    tax.innerHTML = `${taxFee.toFixed(2)}`;
+    shownGallons.innerHTML = `${gallons.toFixed(2)}`;
+  } else {
+    costDisplay.innerHTML = "--";
+    price.innerHTML = "--";
+    delivery.innerHTML = "--";
+    total.innerHTML = "--";
+    tax.innerHTML = "--";
+    shownGallons.innerHTML = "--";
+  }
+
+  //debugging log: updated values
   console.log("Estimate: " + costDisplay.innerHTML);
   console.log("Delivery: " + delivery.innerHTML);
   console.log("Gallons: " + shownGallons.innerHTML);
@@ -56,70 +73,106 @@ function updateFuelQuote() {
   console.log("Price: " + price.innerHTML);
 }
 
+/*----------Set Date----------*/
+function validateDate() {
+  const curr_day = new Date();
+  const adjustedDay = new Date(
+    curr_day.getFullYear(),
+    curr_day.getMonth(),
+    curr_day.getDate(),
+    0,
+    0,
+    0,
+    0
+  );
+
+  const selectedDate = new Date(
+    document.getElementById("delivery_date").value + "T00:00:00"
+  );
+  // selectedDate.setHours(0, 0, 0, 0);
+  // const dateString = document.getElementById("delivery_date").value;
+  // const [year, month, day] = dateString.split("-");
+  // const selectedDate = new Date(Date.UTC(year, month - 1, day));
+  // const selectedDate = new Date(document.getElementById("delivery_date").value);
+  // selectedDate.setHours(0, 0, 0, 0);
+
+  // console.log("Current Date: " + curr_day.toLocaleDateString());
+  // console.log("Adjusted Date: " + adjustedDay.toLocaleDateString());
+  console.log("Selected Date: " + selectedDate.toLocaleDateString());
+
+  if (selectedDate <= adjustedDay) {
+    console.log("Error Date: " + selectedDate.toLocaleDateString());
+    console.log("Adjusted Date: " + adjustedDay.toLocaleDateString());
+    console.log("Current Date: " + curr_day.toLocaleDateString());
+    document.getElementById("date_error").style.display = "inline";
+    return false;
+  }
+
+  document.getElementById("date_error").style.display = "none";
+  return true;
+}
+
+function dateSelection() {
+  const day = new Date();
+  const dd = String(day.getDate()).padStart(2, "0");
+  const mm = String(day.getMonth() + 1).padStart(2, "0");
+  const yyyy = day.getFullYear();
+  const today = mm + "-" + dd + "-" + yyyy;
+  document.getElementById("delivery_date").min = today;
+
+  const isValidDate = validateDate();
+  if (isValidDate) {
+    updateFuelQuote();
+  }
+}
+
+function gallonDateValidation() {
+  const isValidDate = validateDate();
+  if (isValidDate) {
+    updateFuelQuote();
+  }
+  console.log(isValidDate);
+}
+
+fuelGallons.addEventListener("input", gallonDateValidation);
+delivery_dateInput.addEventListener("input", gallonDateValidation);
+
 //making it interactive? once the desired price w desired date is found, click get quote to save the quote
-//fuelGallons.addEventListener("input", validateForm);
-//fuelGallons.addEventListener("input", updateFuelQuote);
-//delivery_dateInput.addEventListener("input", updateFuelQuote);
+// fuelGallons.addEventListener("input", validateForm);
+// fuelGallons.addEventListener("input", updateFuelQuote);
+// delivery_dateInput.addEventListener("input", updateFuelQuote);
+// delivery_dateInput.addEventListener("input", dateSelection);
 
 // this function makes sure that both date and gallon are filled in before displaying the quote and its
 //interactive, changing the gallon changes the price and we might need to implement changing the date
 //also change the price.
-function validateAndUpdateQuote() {
-  const gallonsValue = fuelGallons.value.trim();
-  const deliveryDateValue = delivery_dateInput.value.trim();
+// function validateAndUpdateQuote() {
+//   const deliveryDateValue = delivery_dateInput.value.trim();
 
-  
-  const isValidGallons = validateForm();
-  //may want to check if the date is in right format but may not need too cuz we use calander
-  const isValidDeliveryDate = deliveryDateValue !== '';
+//   const isValidGallons = validateForm();
+//   //may want to check if the date is in right format but may not need too cuz we use calander
+//   const isValidDeliveryDate = deliveryDateValue !== "";
 
-  if (isValidGallons && isValidDeliveryDate) {
-    updateFuelQuote();
-  }
-
-}
-
-// Attach validateAndUpdateQuote to the input event listeners
-fuelGallons.addEventListener("input", validateAndUpdateQuote);
-delivery_dateInput.addEventListener("input", validateAndUpdateQuote);
-
-/*----------Button Submit----------*/
-const button = document.getElementById("button");
-// may have to fix this logic 
- button.addEventListener("click", function() {
-  
-   if(validateForm()) {
-     updateFuelQuote();
-   }
- });
-
-function cardSubmit() {}
-
-// button.addEventListener("click", cardSubmit);
-
-/*----------Set Date----------*/
-// function dateSelection() {
-//   const day = new Date();
-//   const dd = String(day.getDate()).padStart(2, "0");
-//   const mm = String(day.getMonth() + 1).padStart(2, "0");
-//   const yyyy = day.getFullYear();
-//   const today = mm + "-" + dd + "-" + yyyy;
-//   const selectedDate = document.getElementById("delivery_date");
-//   document.getElementById("delivery_date").setAttribute("min", today);
-//   document.getElementById("date_error").style.display = "none";
-
-//   if (selectedDate < day) {
-//     console.log("Error Date: " + selectedDate.innerHTML);
-//     document.getElementById("date_error").style.display = "inline";
-//     return false;
+//   if (isValidGallons && isValidDeliveryDate) {
+//     updateFuelQuote();
 //   }
-//   document.getElementById("date_error").style.display = "none";
-
-//   console.log(today);
-//   // console.log("Error Date: " + selectedDate.innerHTML);
-//   return true;
 // }
 
-// document.addEventListener("DOMContentLoaded", dateSelection);
+// // Attach validateAndUpdateQuote to the input event listeners
+// fuelGallons.addEventListener("input", validateAndUpdateQuote);
+// delivery_dateInput.addEventListener("input", validateAndUpdateQuote);
+
+/*----------Button Submit----------*/
+// const button = document.getElementById("button");
+// // may have to fix this logic
+// button.addEventListener("click", function () {
+//   if (validateForm()) {
+//     updateFuelQuote();
+//   }
+// });
+
+// function cardSubmit() {}
+
+// button.addEventListener("click", cardSubmit);
 
 /*-------------------Fuel Quote Update-------------------*/

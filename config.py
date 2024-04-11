03dad6f8cc8adad98d4d19a8f59426a5ec5482af
Romina <romina.j348@gmail.com>
2024-04-11 16:56:09 -0500

    created the db and its tables, integrated prog to work w db instead of the dummy var

---
+++ config.py
@@ -0,0 +1,4 @@
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = 'postgresql://rominasobhani:romi@localhost/fuelmetrics'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'your_secret_key'

32165cc35930aa3126e6722c87d567b8022afbed
Romina <romina.j348@gmail.com>
2024-04-24 17:35:58 -0500

    this shallll work

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 
 class Config(object):
     # SQLALCHEMY_DATABASE_URI = 'postgresql://rominasobhani:romi@localhost/fuelmetrics'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://bolu:boluu@localhost/fuelmetrics'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://rominasobhani:romi@8.tcp.ngrok.io:16206/fuelmetrics'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'your_secret_key'
 
